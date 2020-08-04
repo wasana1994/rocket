379abfb5d5cdda2c3cad6cb610fd4aa77a7e4431
wasana1994 <69210932+wasana1994@users.noreply.github.com>
2020-08-04 23:45:22 +0700

    Add files via upload

---
+++ src/components/Home.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="row">
+      <div class="col-sm-12">
+        <h1 class="text-center">Rocket Project</h1>
+      </div>
+      <div class="col-sm-12">
+        <ul>
+          <li v-for="item in launchList" :key="item.id">
+            {{ item.name }},{{item.date_local}}
+          </li>
+        </ul>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      launchList: [],
+    }
+  },
+  created(){
+    this.axios.get('https://api.spacexdata.com/v4/launches')
+    .then(response => {
+      // JSON responses are automatically parsed.
+      this.launchList = response.data
+    })
+    .catch(e => {
+      this.errors.push(e)
+    })
+  },
+  mounted(){
+
+  },
+  methods: {
+    
+  }
+}
+</script>
---
+++ src/main.js
@@ -0,0 +1,23 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import VueAxios from "vue-axios";
+import axios from "axios";
+
+Vue.config.productionTip = false
+Vue.use(VueAxios, axios);
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
+
+// new Vue({
+//   router,
+//   store,
+//   render: h => h(App)
+// }).$mount('#app');
